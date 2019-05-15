d38d4c24e9abbb7778c97041ba262b6dce0cd642
Svichar <vsvich@Development.stationcasinos.net>
2019-05-15 16:48:00 -0700

    Initial commit for angular-complete

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { template } from '@angular/core/src/render3';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html'
+})
+export class HeaderComponent{
+
+}
